cce4fdf726eebc3fb864fd0e84612694e36068f6
alecxz20 <alexisopereze@gmail.com>
2023-09-28 19:35:06 -0500

    new repository in github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-udemy/",
+  base: '/movies-react-js/',
   plugins: [react()],
 })
